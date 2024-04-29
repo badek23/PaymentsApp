46dcee64d7d56a2fc2480a5a6e737085224e07b9
Kate <kate.bade@student.ie.edu>
2024-04-29 16:26:10 +0200

    dict to df

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ amount = st.number_input('Amount')
 # bar = st.slider("bar", 0, 100)
 
 if st.button("Add row"):
-    data = data.append({"Person Who Paid": user_paid, "Person Who Owes": user_topay, "Item": item, "Amount": amount}, ignore_index=True)
+    data = pd.concat([data, pd.DataFrame({"Person Who Paid": user_paid, "Person Who Owes": user_topay, "Item": item, "Amount": amount})], ignore_index=True)
+    #data = data.append({"Person Who Paid": user_paid, "Person Who Owes": user_topay, "Item": item, "Amount": amount}, ignore_index=True)
     save_data(data)
     st.write("Owed money successfully added.")
 
